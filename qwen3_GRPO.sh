e888dc4b7bb6a627728f76a85f8172520d437ae3
LIAO-JIAN-PENG <alan36257407@gmail.com>
2025-02-24 13:03:48 +0800

    Add training scripts and update .gitignore
    * training script for qwen3, grpo

---
+++ qwen3_GRPO.sh
@@ -0,0 +1,9 @@
+export N_GPUS=2
+export BASE_MODEL="models/Qwen2.5-3B"
+export DATA_DIR="data"
+export ROLLOUT_TP_SIZE=2
+export EXPERIMENT_NAME=countdown-qwen2.5-3b-beta0
+export VLLM_ATTENTION_BACKEND=XFORMERS
+export HYDRA_FULL_ERROR=1
+
+bash ./scripts/train_grpo.sh
